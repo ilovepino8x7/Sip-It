b38f378b8ccd5458820e33215f3355be94d7c024
ilovepino8x7 <teddybhunt@gmail.com>
2025-12-01 16:35:53 +0000

    Basic Line Drawing
    I created basic line drawing, after clicking the line begins drawing and updating but never finishes (adding next)

--- drawing/Assets/lineDrawer.cs
+++ drawing/Assets/lineDrawer.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class lineDrawer : MonoBehaviour
 {
+    public GameObject linePrefab;
+    private bool drawing = false;
+    private GameObject line;
+    private List<Vector3> points = new List<Vector3>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,30 @@ public class lineDrawer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && !drawing)
+        {
+            drawing = true;
+            MakeLine();
+        }
+        if (drawing)
+        {
+            UpdateLine();
+            if (points.Count >= 2)
+            {
+                line.GetComponent<LineRenderer>().positionCount = points.Count;
+                line.GetComponent<LineRenderer>().SetPositions(points.ToArray());
+            }
+        }
+    }
+    private void MakeLine()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        line = Instantiate(linePrefab);
+        points.Add(mousePos);
+    }
+    private void UpdateLine()
+    {
+        Vector2 newMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        points.Add(newMousePos);
     }
 }
